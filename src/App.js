21bef190002f9f60766f72d616cb4570a8e0211c
kianoushit1999 <kianoushit1999@gmail.com>
2024-06-02 11:03:34 +0330

    basic layout for different parts of the project is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Form from "./Form";
 import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
 
 function App() {
   return (
     <div className="App">
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
