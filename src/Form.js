5d12eab71338f4fe6726da76ac2747b898fdae8b
kianoushit1999 <kianoushit1999@gmail.com>
2024-06-03 18:51:36 +0330

    complete form but these items need to be showed up

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,33 @@
 
+import { useState } from "react";
+
 function Form() {
+
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
 
     function submitHandler(e) {
         e.preventDefault();
+
+        if (!description) return
+
         console.log(e.target[1].value);
     }
 
     return <div className='add-form'>
         <h3>What do you need for your trip ?</h3>
         <form onSubmit={submitHandler}>
-            <select>
+            <select value={quantity} onChange={(e)=> {
+                setQuantity(Number(e.target.value));
+            }}>
                 {Array.from({length:20}, (_, index) => index + 1).map((val, index, array) => {
                     return <option key={val}>{val}</option>
                 })}
             </select>
 
-            <input type="text" placeholder="Item ..." />
+            <input type="text" placeholder="Item ..." value={description} onChange={(e) => {
+                setDescription(e.target.value);
+            }} />
             <button>Add</button>
         </form>
     </div>
